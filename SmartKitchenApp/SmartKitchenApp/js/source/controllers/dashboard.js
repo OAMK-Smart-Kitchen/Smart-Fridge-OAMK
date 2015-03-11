783b5e129ee83eca912b71730b4df0c5d561bbd5
Bernd Verhofstadt <bernd.verhofstadt@student.ap.be>
2015-03-11 17:19:30 +0200

    Weather Service added
    Powered by Forecast.io

--- SmartKitchenApp/SmartKitchenApp/js/source/controllers/dashboard.js
+++ SmartKitchenApp/SmartKitchenApp/js/source/controllers/dashboard.js
@@ -3,7 +3,8 @@ app.controller('dashboard', [
     '$location',
     '$rootScope',
     'geolocation',
-    function ($scope, $location, $rootScope, geolocation) {
+    'weatherservice',
+    function ($scope, $location, $rootScope, geolocation, weatherservice) {
 
         /*
         Stap1: functie Init
@@ -14,44 +15,73 @@ app.controller('dashboard', [
 
         var init = function () {
             console.log('dashboard controller started');
-            
+            getWeatherForecast();
         };
 
         /*
         Stap2: Controller vars (niet in scope!)
         ------------------------------------------
         */
-        
 
+        //var mysrclat= 0; var mysrclong = 0;
+
+        //$scope.coords = function($scope) {
+        //    if (navigator.geolocation) {
+        //        navigator.geolocation.getCurrentPosition(function (position) {
+        //            mysrclat = position.coords.latitude; 
+        //            mysrclong = position.coords.longitude;
+        //            console.log(mysrclat);
+        //            console.log(mysrclong);
+        //        });
+
+        //    }
+
+
+        //var coords = geolocation.getLocation().then(function (data) {
+        //    console.log("LAT: " + data.coords.latitude);
+        //    console.log("LONG: " + data.coords.longitude);
+        //    return { lat: data.coords.latitude, long: data.coords.longitude };
+        //});
 
         /*
         Stap3: Controller functions (niet in scope!)
         --------------------------------------------
         */
 
-        var weatherForecast = function () {
-            return { monday: "0", tuesday: "2"};
+
+
+        var getWeatherForecast = function () {
+            var lati = 0; var longi = 0;
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    lati = position.coords.latitude;
+                    longi = position.coords.longitude;
+                    console.log(lati);
+                    console.log(longi);
+                    weatherservice.getWeather(lati, longi)
+                       .then(function (data) {
+
+                           console.log(data);
+
+                       });
+                });
+
+            };
         };
-     
+
         /*
         Stap4: Scope vars
         ------------------
         */
 
-        $scope.coords = geolocation.getLocation().then(function (data) {
-            console.log("LAT: " + data.coords.latitude);
-            console.log("LONG: " + data.coords.longitude);
-            return { lat: data.coords.latitude, long: data.coords.longitude };
-        });
-
-        $scope.weather = weatherForecast();
+        $scope.weather;
 
         /* Stap5: Scope functions
         -------------------------
         */
-        
 
-       
+
+
         /* Stap6: init aanroepen
         --------------------
         */
