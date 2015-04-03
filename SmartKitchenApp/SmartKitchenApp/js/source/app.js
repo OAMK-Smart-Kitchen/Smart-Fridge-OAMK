ac8c93dd0eeecfd5048410a0ff54ceb7c8f6aa1a
Bernd Verhofstadt <bernd.verhofstadt@student.ap.be>
2015-04-03 19:03:24 +0300

    Login and register UI created

--- SmartKitchenApp/SmartKitchenApp/js/source/app.js
+++ SmartKitchenApp/SmartKitchenApp/js/source/app.js
@@ -1,75 +1,33 @@
 var app = angular.module('SmartKitchenApp', ['ngRoute', 'ngResource', 'ui.bootstrap', 'jsonDate', 'typeFilter', 'geolocation']);
 
-//app.serviceUrl = 'http://partibookservice.cloudapp.net/service.svc/';
-//app.blobUrl = 'https://partibookservice.blob.core.windows.net/images/';
+app.serviceUrl = 'http://api.verhofstadt.eu/api/';
+//app.blobUrl = 'http://urltopictures.com';
 app.CryptoJS = function (message) {
     return CryptoJS.SHA256(message);
 }
 
 //dataTemplates
-//Teacher Object
+//Member Object
 app.Member = function () {
     this.Id = null;
-    this.Username = null;
-    this.FirstName = null;
-    this.Lastname = null;
-    this.Email = null;
-    this.Password = null;
-};
-app.CurrentMember = new app.Member();
-//Lesson Object
-app.Lesson = function () {
-    this.Id = null;
-    this.Name = null;
-    this.CreationDate = null;
-    this.RevisionDate = null;
-    this.IsActive = null;
-    this.Token = null;
-    this.Teacher_Id = null;
-}
-app.CurrentLesson = new app.Lesson();
-//Question Object
-app.Question = function () {
-    this.Id = null;
-    this.Text = null;
-    this.Image = null;
-    this.IsActive = null;
-    this.QuestionType = null;
-    this.Lesson_Id = null;
-    this.ImageString = null;
-}
-app.CurrentQuestion = new app.Question();
-//PossibleAnswer Object
-app.PossibleAnswer = function () {
-    this.Id = 0;
-    this.Text = null;
-    this.Image = null;
-    this.IsCorrect = false;
-    this.Question_Id = null;
-    this.AnswerImage = null;
-}
-app.CurrentPossibleAnswer = new app.PossibleAnswer();
-//CollectedAnswer Object
-app.CollectedAnswer = function () {
     this.Firstname = null;
     this.Lastname = null;
-    this.Answer = null;
-    this.AnswerImage = null;
-    this.Token = null;
-    this.Question_Id = null;
-    this.AnswerImageString = null;
-}
-//Type Enum
-app.TypeEnum = {
-    Test1: 0,
-    Test2: 1,
-    Test3: 2,
-    Test4: 3
+    this.DateOfBirth = null;
+    this.Email = null;
+    this.Password = null;
+    this.PictureUrl = null;
+    this.DefaultColor = null;
+    this.Active = null;
+    this.GameActivated = null;
+    this.GameHealthLevel = null;
+    this.GamePoints = null;
+    this.Admin = null;
 };
-
-//FILTERS
-app.filter('slice', function () {
-    return function (arr, start, end) {
-        return arr.slice(start, end);
-    };
-});
+app.CurrentMember = new app.Member();
+//Kitchen Object
+app.Kitchen = function () {
+    this.Id = null;
+    this.Name = null;
+    this.TemperatureFridge = null;
+}
+app.CurrentKitchen = new app.Kitchen();
--- SmartKitchenApp/SmartKitchenApp/js/source/controllers/login.js
+++ SmartKitchenApp/SmartKitchenApp/js/source/controllers/login.js
@@ -25,28 +25,86 @@ app.controller('login', [
         Stap3: Controller functions (niet in scope!)
         --------------------------------------------
         */
-
+        var resetRegisterForm = function () {
+            $scope.reg_section_one = true;
+            $scope.reg_section_two = false;
+            $scope.reg_section_three = false;
+            $scope.Member.Password = null;
+            $scope.Member.Email = null;
+            $scope.Member.Firstname = null;
+            $scope.Member.Lastname = null;
+            $scope.Member.DateOfBirth = null;
+            $scope.Kitchen.Name = null;
+        };
 
         /*
         Stap4: Scope vars
         ------------------
         */
+        // Status options
+        // 0 : Nothing happend everything is fine
+        // 1 : Processing 
+        // 2 : Failed-Error
+        // 3 : Success
 
-        $scope.loginFailed = false;
         $scope.loginStatus = 0;
+
+        $scope.registerStatus = 0;
+
+        $scope.showlogin = true;
+        $scope.showregister = false;
+
+        $scope.reg_section_one = true;
+        $scope.reg_section_two = false;
+        $scope.reg_section_three = false;
+
 
         /* Stap5: Scope functions
         -------------------------
         */
+        // Toggle Login-Register tabs
+        $scope.switchTo = function (name) {
+            if (name == 'login') {
+                resetRegisterForm();
 
+                $scope.showlogin = true;
+                $scope.showregister = false;
+            }
+            else
+                if (name == 'register') {
+                    $scope.showlogin = false;
+                    $scope.showregister = true;
+                }
+        };
+
+        // Register Member with kitchen
+        $scope.kitchenDone = function (isValid) {
+            if (isValid) {
+                $scope.reg_section_one = false;
+                $scope.reg_section_two = true;
+                $scope.reg_section_three = false;
+                console.log("Kitchen data success");
+            }
+        };
+
+        $scope.adminDone = function (isValid) {
+            if (isValid) {
+                $scope.reg_section_one = false;
+                $scope.reg_section_two = false;
+                $scope.reg_section_three = true;
+                console.log("Member data register success");
+            }
+        };
 
 
         //NAVIGATION
-        $scope.goToDashboard = function () {
-            //$location.path("/dashboard/:kitchen.id/member");
-            $location.path("/members");
-            console.log("Navigate to Member Dasboard");
-            console.log("Kitchen ID: " + kitchen.id);
+        $scope.goToDashboard = function (isValid) {
+            if (isValid) {
+                //$location.path("/dashboard/:kitchen.id/member");
+                $location.path("/members");
+                console.log("Navigate to Member Dasboard");
+                console.log("Kitchen ID: " + kitchen.id);
+            }
         };
 
         $scope.goToRegister = function () {
