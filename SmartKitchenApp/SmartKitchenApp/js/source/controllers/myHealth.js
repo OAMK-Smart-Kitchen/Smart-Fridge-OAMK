8b9be87bb8b53b0f3499825a2ebc1cb7b3854f43
Bernd Verhofstadt <bernd.verhofstadt@student.ap.be>
2015-04-05 13:51:15 +0300

    My Health UI added

---
+++ SmartKitchenApp/SmartKitchenApp/js/source/controllers/myHealth.js
@@ -0,0 +1,56 @@
+app.controller('myhealth', [
+    '$scope',
+    '$location',
+    '$rootScope',
+    function ($scope, $location, $rootScope) {
+
+        /*
+        Stap1: functie Init
+        --------------------
+        */
+
+        var init = function () {
+            console.log('My Health controller started');
+        };
+
+        /*
+        Stap2: Controller vars (niet in scope!)
+        ------------------------------------------
+        */
+        
+
+
+        /*
+        Stap3: Controller functions (niet in scope!)
+        --------------------------------------------
+        */
+
+
+     
+        /*
+        Stap4: Scope vars
+        ------------------
+        */
+
+
+
+        /* Stap5: Scope functions
+        -------------------------
+        */
+        
+        $scope.goToBMI = function () {
+            console.log("Navigate to My BMI");
+            $location.path("/dashboard/myhealth/bmi");
+        };
+
+        $scope.goToPoints = function () {
+            console.log("Navigate to My Points");
+            $location.path("/dashboard/myhealth/points");
+        };
+       
+        /* Stap6: init aanroepen
+        --------------------
+        */
+        init();
+
+    }]);
