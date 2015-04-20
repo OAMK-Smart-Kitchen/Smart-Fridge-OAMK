07118699ea492c4114729b22bfcb40b7d955cb52
Tommampaey <mampaeytom@hotmail.com>
2015-04-20 10:48:47 +0200

    Working LEDDRIVER over I2C

---
+++ SmartKitchen-Emdebbed System/Software/Arduino/Testing/New_algo_NFC-read/New_algo_NFC-read.ino
@@ -0,0 +1,119 @@
+// Smart Fridge
+// Tom Mampaey
+/*
+Test-code to detect and convert the ID of an NFC-tag too a usable int.
+This int will be sended serial to the computer and the he will compare it with the numbers in the database.
+ */
+
+// NFC
+int incomingByte = 0;   // for incoming serial data
+String productID = "";
+String tempString = "";
+int minLengthID = 40;  // minimale lengte van NFC-tag ID
+
+// NFC-ID's
+const String StartRef = "191183";
+const String Banana = "1188611854245125125125141153151143159229235";
+const String Cucumber = "118150150205245125125125141153151143159229235";
+const String Vegetables = "95951741188653153229235095951741188653153229235";
+const String Milk = "1185424654237125125125141153151143159229235";
+const String Ketchup = "118246147115125159125125141153151143159229235";
+const String NokiaID = "1019417886611451252292350101941788661145125229235";
+const String PhoneTom = "21424618224615024611886235021424618224615024611886235";
+const String PhoneHenry = "21424618224615024611886235021424618224615024611886235";
+
+/*
+#define RGB_ADDRESS_M1 57
+#define RGB_ADDRESS_M2
+#define RGB_ADDRESS_M3
+#define RGB_ADDRESS_M4
+#define RGB_ADDRESS_M5
+*/
+
+// LED - driver
+#include <Wire.h>
+byte iInput = 0;
+byte iOutput = 0;
+
+void setup() {
+  // Serials
+  Serial.begin(9600);
+
+  //I2C
+  Wire.begin();
+}
+
+void loop()
+{
+  // ----- Switch-control -----
+ /* Wire.beginTransmission(58);  //Begin transmission to PCF8574 to activate RX (address: 111010)
+  Wire.write(1);         //Send data to PCF8574 (with the LEDs)
+  Wire.endTransmission();      //End Transmission to PCF8574 (with the LEDs)
+  // ----- End Switch-control -----
+  Wire.requestFrom(57, 1); // Begin transmission to PCF8574 with the buttons */
+
+  // ----- NFC-Tag Detection -----
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    /*if (productID == StartRef)
+    {
+      Serial.print("NFC-ID: ");
+      // Software on computer will do this in futere (Database) /*
+      if (productID == Banana)
+      {
+        Serial.println("Banana");
+      }
+      else if (productID == Cucumber)
+      {
+        Serial.println("Cucumber");
+        iOutput = 2;
+      }
+      else if (productID == Vegetables)
+      {
+        Serial.println("Box with vegetables");
+      }
+      else if (productID == Milk)
+      {
+        Serial.println("Milk");
+      }
+      else if (productID == Ketchup)
+      {
+        Serial.println("Ketchup");
+        iOutput = 1;
+      }
+      else if (productID == NokiaID)
+      {
+        Serial.println("Why is there a Nokia in your fridge?");
+      }
+      else
+      {
+        Serial.println(productID);
+      }
+      productID = "";
+    }
+    else
+    {
+      tempString = String(incomingByte);
+      productID = productID + tempString;
+
+    }
+  }
+*/
+Serial.print(incomingByte);
+}
+/*tempString = String(incomingByte);
+productID = productID + tempString;
+if (productID == StartRef)
+    {Serial.println('YES');}
+
+}
+/*
+  Wire.beginTransmission(56);  //Begin transmission to PCF8574 (with the LEDs)
+  Wire.write(0);         //Send data to PCF8574 (with the LEDs)
+  Wire.endTransmission();      //End Transmission to PCF8574 (with the LEDs)*/
+}
+
+
+
+
+
--- SmartKitchen-Emdebbed System/Software/Arduino/Testing/_7-LED_colors/_7-LED_colors.ino
+++ SmartKitchen-Emdebbed System/Software/Arduino/Testing/_7-LED_colors/_7-LED_colors.ino
@@ -3,10 +3,12 @@ byte iInput = 0;
 byte iOutput = 0;
 int incomingByte = 0;
 
+#define off 0
 #define green 1
 #define red 2
-#define grbl 3
+#define yellow 3
 #define blue 4
+#define brblue 4
 #define purple 6
 #define white 7
 
@@ -23,7 +25,7 @@ void loop() {
     Wire.beginTransmission(56);  //Begin transmission to PCF8574 (with the LEDs)
     Wire.write(i);         //Send data to PCF8574 (with the LEDs)
     Wire.endTransmission();      //End Transmission to PCF8574 (with the LEDs)
-    delay(1000);
+    delay(2000);
   }
 
   //incomingByte = Serial.read();
