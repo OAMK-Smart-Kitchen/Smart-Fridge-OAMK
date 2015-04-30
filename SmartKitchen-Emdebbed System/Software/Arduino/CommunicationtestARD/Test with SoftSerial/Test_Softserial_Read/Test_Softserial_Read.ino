f4a14f0abdf7b2d6292f8bdf1aeaeb8f9b938f8f
Tommampaey <mampaeytom@hotmail.com>
2015-04-30 14:45:53 +0200

    testing serial connection between two DUE's

--- SmartKitchen-Emdebbed System/Software/Arduino/CommunicationtestARD/Test with SoftSerial/Test_Softserial_Read/Test_Softserial_Read.ino
+++ SmartKitchen-Emdebbed System/Software/Arduino/CommunicationtestARD/Test with SoftSerial/Test_Softserial_Read/Test_Softserial_Read.ino
@@ -1,13 +1,19 @@
-#include <SoftwareSerial.h>
+int incomingByte = 0;   // for incoming serial data
 
-SoftwareSerial mySerial(10,11);
-
-void setup()
-{
-  mySerial.begin(9600);
+void setup() {
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
 }
 
-void loop()
-{
-  char c = mySerial.read();
+void loop() {
+
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte, DEC);
+        }
 }
+ 
