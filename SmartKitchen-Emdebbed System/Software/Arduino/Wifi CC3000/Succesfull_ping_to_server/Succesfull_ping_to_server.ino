a52fa4f1cb51f0b24d0b82dc8d0720e6baac752d
Tommampaey <mampaeytom@hotmail.com>
2015-05-13 13:00:47 +0200

    Testing (and succes) Wifi-connection

--- SmartKitchen-Emdebbed System/Software/Arduino/Wifi CC3000/Succesfull_ping_to_server/Succesfull_ping_to_server.ino
+++ SmartKitchen-Emdebbed System/Software/Arduino/Wifi CC3000/Succesfull_ping_to_server/Succesfull_ping_to_server.ino
@@ -1,14 +1,14 @@
 /****************************************************************
-WebClient.ino
-CC3000 WebClient Test
+PingTest.ino
+CC3000 Ping Test
 Shawn Hymel @ SparkFun Electronics
-March 1, 2014
+February 1, 2014
 https://github.com/sparkfun/SFE_CC3000_Library
 
-Manually connects to a WiFi network and performs an HTTP GET
-request on a web page. Prints the contents of the page to
-the serial console.
-
+Connects to the access point given by the SSID and password and
+waits for a DHCP-assigned IP address. Pings the give website or
+IP address and waits for a response.
+ 
 The security mode is defined by one of the following:
 WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA, WLAN_SEC_WPA2
 
@@ -26,7 +26,7 @@ Hardware Connections:
  13         SCK             SPI Clock
 
 Resources:
-Include SPI.h, SFE_CC3000.h, and SFE_CC3000_Client.h
+Include SPI.h and SFE_CC3000.h
 
 Development environment specifics:
 Written in Arduino 1.0.5
@@ -38,10 +38,9 @@ buy us a round!
 
 Distributed as-is; no warranty is given.
 ****************************************************************/
-
+ 
 #include <SPI.h>
 #include <SFE_CC3000.h>
-#include <SFE_CC3000_Client.h>
 
 // Pins
 #define CC3000_INT      2   // Needs to be an interrupt pin (D2/D3)
@@ -53,24 +52,28 @@ Distributed as-is; no warranty is given.
 
 // Constants
 char ap_ssid[] = "SmartKitchen";                  // SSID of network
-char ap_password[] = "It's all about food!";          // Password of network
+char ap_password[] = "12345678";          // Password of network
 unsigned int ap_security = WLAN_SEC_WPA2; // Security of network
 unsigned int timeout = 30000;             // Milliseconds
-char server[] = "www.example.com";        // Remote host site
+char remote_host[] = "api.verhofstadt.eu";  // Host to ping
+unsigned int num_pings = 3;    // Number of times to ping
 
 // Global Variables
 SFE_CC3000 wifi = SFE_CC3000(CC3000_INT, CC3000_EN, CC3000_CS);
-SFE_CC3000_Client client = SFE_CC3000_Client(wifi);
 
 void setup() {
   
   ConnectionInfo connection_info;
+  IPAddress ip_addr;
+  IPAddress remote_ip;
+  PingReport ping_report = {0};
   int i;
+  
   // Initialize Serial port
   Serial.begin(115200);
   Serial.println();
   Serial.println("---------------------------");
-  Serial.println("SparkFun CC3000 - WebClient");
+  Serial.println("SparkFun CC3000 - Ping Test");
   Serial.println("---------------------------");
   
   // Initialize CC3000 (configure SPI communications)
@@ -79,9 +82,9 @@ void setup() {
   } else {
     Serial.println("Something went wrong during CC3000 init!");
   }
-  
-  // Connect using DHCP
-  Serial.print("Connecting to SSID: ");
+
+  // Connect and wait for DHCP-assigned IP address
+  Serial.print("Connecting to: ");
   Serial.println(ap_ssid);
   if(!wifi.connect(ap_ssid, ap_security, ap_password, timeout)) {
     Serial.println("Error: Could not connect to AP");
@@ -91,6 +94,7 @@ void setup() {
   if ( !wifi.getConnectionInfo(connection_info) ) {
     Serial.println("Error: Could not obtain connection details");
   } else {
+    Serial.println("Connected!");
     Serial.print("IP Address: ");
     for (i = 0; i < IP_ADDR_LEN; i++) {
       Serial.print(connection_info.ip_address[i]);
@@ -101,48 +105,66 @@ void setup() {
     Serial.println();
   }
   
-  // Make a TCP connection to remote host
-  Serial.print("Performing HTTP GET of: ");
-  Serial.println(server);
-  if ( !client.connect(server, 80) ) {
-    Serial.println("Error: Could not make a TCP connection");
+  // Perform a DNS lookup to get the IP address of a host
+  Serial.print("Looking up IP address of: ");
+  Serial.println(remote_host);
+  if ( !wifi.dnsLookup(remote_host, &remote_ip) ) {
+    Serial.println("Error: Could not lookup host by name");
+  } else {
+    Serial.print("IP address found: ");
+    for (i = 0; i < IP_ADDR_LEN; i++) {
+      Serial.print(remote_ip[i], DEC);
+      if ( i < IP_ADDR_LEN - 1 ) {
+        Serial.print(".");
+      }
+    }
+    Serial.println();
   }
   
-  // Make a HTTP GET request
-  client.println("GET /index.html HTTP/1.1");
-  client.print("Host: ");
-  client.println(server);
-  client.println("Connection: close");
-  client.println();
-  Serial.println();
+  // Ping IP address of remote host
+  Serial.print("Pinging ");
+  for (i = 0; i < IP_ADDR_LEN; i++) {
+    Serial.print(remote_ip[i], DEC);
+    if ( i < IP_ADDR_LEN - 1 ) {
+      Serial.print(".");
+    }
+  }
+  Serial.print(" ");
+  Serial.print(num_pings, DEC);
+  Serial.println(" times...");
+  if ( !wifi.ping(remote_ip, ping_report, 3, 56, 1000) ) {
+    Serial.println("Error: no ping response");
+  } else {
+    Serial.println("Pong!");
+    Serial.println();
+    Serial.print("Packets sent: ");
+    Serial.println(ping_report.packets_sent);
+    Serial.print("Packets received: ");
+    Serial.println(ping_report.packets_received);
+    Serial.print("Min round time (ms): ");
+    Serial.println(ping_report.min_round_time);
+    Serial.print("Max round time (ms): ");
+    Serial.println(ping_report.max_round_time);
+    Serial.print("Avg round time (ms): ");
+    Serial.println(ping_report.avg_round_time);
+    Serial.println();
+  }
+  
+  // Disconnect
+  if ( wifi.disconnect() ) {
+    Serial.println("Disconnected");
+  } else {
+    Serial.println("Error: Could not disconnect from network");
+  }
+  
+  // Done!
+  Serial.println("Finished ping test");
+  
 }
 
 void loop() {
   
-  // If there are incoming bytes, print them
-  if ( client.available() ) {
-    char c = client.read();
-    Serial.print(c);
-  }
+  // Do nothing
+  delay(1000);
   
-  // If the server has disconnected, stop the client and wifi
-  if ( !client.connected() ) {
-    Serial.println();
-    
-    // Close socket
-    if ( !client.close() ) {
-      Serial.println("Error: Could not close socket");
-    }
-    
-    // Disconnect WiFi
-    if ( !wifi.disconnect() ) {
-      Serial.println("Error: Could not disconnect from network");
-    }
-    
-    // Do nothing
-    Serial.println("Finished WebClient test");
-    while(true){
-      delay(1000);
-    }
-  }
 }
