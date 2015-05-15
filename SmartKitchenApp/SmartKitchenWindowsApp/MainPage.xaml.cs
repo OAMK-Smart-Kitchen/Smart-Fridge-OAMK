37df169067ca3acd6896414c101763f050c80ba3
Bernd Verhofstadt <bernd.verhofstadt@student.ap.be>
2015-05-15 18:22:27 +0300

    Windows App created with WebView for webpage

---
+++ SmartKitchenApp/SmartKitchenWindowsApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SmartKitchenWindowsApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadAngularApp();
+        }
+
+        public void LoadAngularApp()
+        {
+            try
+            {
+                AngularApp.Navigate(new Uri("http://app.verhofstadt.eu/smartfridge/"));
+            }
+            catch (Exception exc)
+            {
+                var messageDialog = new MessageDialog("No internet connection has been found.");
+                messageDialog.ShowAsync();
+            } 
+        }
+       
+    }
+}
